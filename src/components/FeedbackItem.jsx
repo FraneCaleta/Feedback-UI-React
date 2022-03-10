0dd5e3ed7495c08cb4399afd77c435822a521d12
FraneCaleta <frane.caleta@yahoo.com>
2022-03-10 23:55:51 +0100

    FeedbackList component and data

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-import { useState } from "react";
 
-function FeedbackItem() {
-  const [rating, setRating] = useState(7);
-  const [text, setText] = useState("This is an example");
-
+function FeedbackItem({ item }) {
   return (
     <div className="card">
-      <div className="num-display">{rating}</div>
-      <div className="text-display">{text}</div>
+      <div className="num-display">{item.rating}</div>
+      <div className="text-display">{item.text}</div>
     </div>
   );
 }
