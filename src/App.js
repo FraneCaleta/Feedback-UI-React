d2f2995a659d20c6a00b6eed2ac487e4a83aeb1c
FraneCaleta <frane.caleta@yahoo.com>
2022-03-10 23:45:57 +0100

    Add Feedback Item

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from "./components/Header";
+import FeedbackItem from "./components/FeedbackItem";
 
 function App() {
   return (
     <>
       <Header />
+      <div className="container">
+        <FeedbackItem />
+      </div>
     </>
   );
 }
